e369be4c63a0bc0541e5083695551e4f196cdbdd
Benedikt Schmied <development@benedikt-schmied.de>
2017-10-31 13:37:08 +0100

    minor error correction in pushing into tables functions it is forbidden to use the 'group' identifier

--- bm_database/bm_database.py
+++ bm_database/bm_database.py
@@ -5,27 +5,31 @@ import sqlite3
 import os
 
 def main():
-    setup_db()
+    c=setup_db()
+    push_into_members(c, "Horst", 1)
+    push_into_members(c, "Keving", 1)
+    push_into_members(c, "Lolita", 1)
+    show_all_members(c)
     
 def setup_db():
     print("setting up the database as well as the tables")
     conn = sqlite3.connect("budget_management.db")
     c=conn.cursor()
     
-    for i in range(0,4):
+    for i in range(0,5):
         try:
             if i == 0:
-                c.execute("CREATE TABLE members (id integer primary key, name text unique, group integer)")
+                c.execute("CREATE TABLE members (id integer primary key, name text, group_of_members integer)")
             elif i == 1:
-                c.execute("CREATE TABLE matter_of_expense (id integer primary key, name text unique, originator integer, provider name, group integer, amount float)")
+                c.execute("CREATE TABLE matter_of_expense (id integer primary key, name text, originator integer, provider name, group_of_expenses integer, amount float)")
             elif i == 2:
                 c.execute("CREATE TABLE invoices (id integer primary key, matter_of_expense integer, integer originator, date text)")
             elif i == 3:
                 c.execute("CREATE TABLE groups_of_expenses (id integer primary key, name text)")
             elif i == 4:
                 c.execute("CREATE TABLE groups_of_members (id integer primary key, name text)")    
-        except :
-            print("    this table exists already")
+        except sqlite3.Error as e:
+            print("An error occurred:", e.args[0])
     return c
 
 def push_into_members(_cursor, _name, _group):
@@ -33,18 +37,18 @@ def push_into_members(_cursor, _name, _group):
     '''
     print("    pushing member")
     try:
-        _cursor.execute("INSERT INTO members(name, origin, group) values (?,?)", (_name, _group,))
-    except:
-        print("    dublicate member encountered")                 
+        _cursor.execute("INSERT INTO members(name, group_of_members) values (?,?)", (_name, _group,))
+    except sqlite3.Error as e:
+        print("An error occurred:", e.args[0])                 
 
 def push_into_matter_of_expense(_cursor, _name, _originator, _provider, _group, _amount):
     ''' \brief pushes a new entry into 'members' table
     '''
     print("    pushing 'matter_of_expense'")
     try:
-        _cursor.execute("INSERT INTO matter_of_expense(name, originator, provider, group, amount) values (?,?,?,?,?)", (_name, _originator, _provider, _group, _amount,))
-    except:
-        print("    dublicate matter_of_expense encountered")      
+        _cursor.execute("INSERT INTO matter_of_expense(name, originator, provider, group_of_expenses, amount) values (?,?,?,?,?)", (_name, _originator, _provider, _group, _amount,))
+    except sqlite3.Error as e:
+        print("An error occurred:", e.args[0])  
         
 def push_into_invoice(_cursor, _matter_of_expense, _originator, _date):
     print("    pushing 'invoices'")
@@ -59,16 +63,24 @@ def push_into_groups_of_members(_cursor, _name):
     print("    pushing 'groups of members'")
     _cursor.execute("INSERT INTO groups_of_members(_name) values (?)", (_name,))
 
-def show_all_members(cursor):
-    for row in cursor.execute("select * from artists"):
+def show_all_members(_cursor):
+    for row in _cursor.execute("select * from members"):
         print(row)
 
-def show_all_matter_of_expense(cursor):
-    for row in cursor.execute("select * from album"):
+def show_all_matter_of_expense(_cursor):
+    for row in _cursor.execute("select * from matter_of_expenses"):
         print(row)
         
-def show_all_invoices(cursor):
-    for row in cursor.execute("select * from track"):
+def show_all_invoices(_cursor):
+    for row in _cursor.execute("select * from invoices"):
+        print(row)
+
+def show_all_groups_of_members(_cursor):
+    for row in _cursor.execute("select * from groups_of_members"):
+        print(row)
+
+def show_all_groups_of_expenses(_cursor):
+    for row in _cursor.execute("select * from groups_of_expenses"):
         print(row)
 
 def get_artist(cursor, name):
@@ -94,8 +106,6 @@ def get_album(cursor, name, artist):
 
 def setup_eyed3():
     print("    setting up the eyed3 plugin")
-
-
     
 if __name__ == "__main__":
     # execute only if run as a script
