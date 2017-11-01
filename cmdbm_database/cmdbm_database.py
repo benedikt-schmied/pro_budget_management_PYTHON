a072d564db1d8a50da16bf502b67c8b4c51ad7d1
Benedikt Schmied <development@benedikt-schmied.de>
2017-11-01 22:06:24 +0100

    last commit for today only minor changes in command line interface

--- cmdbm_database/cmdbm_database.py
+++ cmdbm_database/cmdbm_database.py
@@ -6,18 +6,34 @@ import sqlite3
 import os
 import argparse
 import curses
+from curses import wrapper
 
-def main():
-    print("Hello World")
-    
+def main(stdscr):
+     
     while True:
         c = stdscr.getch()
+        
         if c == ord('p'):
-            PrintDocument()
+            stdscr.addstr(0,0, "printing", curses.A_BOLD)
+            stdscr.refresh()
+        elif c == ord('s'):
+            stdscr.addstr(1,0, "plotting", curses.A_BOLD)
+            stdscr.refresh()
         elif c == ord('q'):
             break  # Exit the while loop
         elif c == curses.KEY_HOME:
             x = y = 0
+
+def build_terminal_session():
+
+    curses.noecho()
+    curses.cbreak()
+    return
+
+def restore_terminal_session(_src):
+    curses.nocbreak()
+    curses.echo()
+    curses.endwin()
 
 if __name__ == "__main__":
     # execute only if run as a script
@@ -27,5 +43,5 @@ if __name__ == "__main__":
     parser.add_argument('user', default='admin', help='who is in charge of making changes or printing lists')
     
     args = parser.parse_args()
-    
-    main()
+    stdscr = curses.initscr()
+    wrapper(main)
