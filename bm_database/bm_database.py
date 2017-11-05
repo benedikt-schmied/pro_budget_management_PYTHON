a0250b9bab8aefba24a6ae45b685acf317042a32
Benedikt Schmied <development@benedikt-schmied.de>
2017-11-05 21:24:37 +0100

    new functions for connetion and disconneting from the database

--- bm_database/bm_database.py
+++ bm_database/bm_database.py
@@ -5,40 +5,71 @@ import sqlite3
 import os
 
 def main():
-    c=setup_db()
-    
-def setup_db():
-    print("setting up the database as well as the tables")
+    ''' 
+    \brief main routine
+    \return void
+    '''
+    c= connect()
+    setup_db(c)
+    disconnect(c)
+
+def connect():
+    ''' 
+    \brief connect to the database
+    '''
     conn = sqlite3.connect("budget_management.db")
-    c=conn.cursor()
+    c = conn.cursor()
+    return c
+
+def disconnect(_cursor):
+    ''' 
+    \brief disconnect from the database
+    '''
+    _cursor.close()
+
+def setup_db(_cursor):
+    ''' 
+    \brief setup the database
     
+    \param _cursor    database cursor
+    '''
+    print("setting up the database as well as the tables")
+   
     for i in range(0,5):
         try:
             if i == 0:
-                c.execute("CREATE TABLE members (id integer primary key, name text, group_of_members integer)")
+                _cursor.execute("CREATE TABLE members (id integer primary key, name text unique, group_of_members integer)")
             elif i == 1:
-                c.execute("CREATE TABLE matter_of_expense (id integer primary key, name text, originator integer, provider name, group_of_expenses integer, amount float)")
+                _cursor.execute("CREATE TABLE matter_of_expense (id integer primary key, name text, originator integer, provider name, group_of_expenses integer, amount float)")
             elif i == 2:
-                c.execute("CREATE TABLE invoices (id integer primary key, matter_of_expense integer, integer originator, date text)")
+                _cursor.execute("CREATE TABLE invoices (id integer primary key, matter_of_expense integer, integer originator, date text)")
             elif i == 3:
-                c.execute("CREATE TABLE groups_of_expenses (id integer primary key, name text)")
+                _cursor.execute("CREATE TABLE groups_of_expenses (id integer primary key, name text unique)")
             elif i == 4:
-                c.execute("CREATE TABLE groups_of_members (id integer primary key, name text)")    
+                _cursor.execute("CREATE TABLE groups_of_members (id integer primary key, name text unique)")    
         except sqlite3.Error as e:
             print("An error occurred:", e.args[0])
-    return c
+    return _cursor
 
 def push_into_members(_cursor, _name, _group):
-    ''' \brief pushes a new entry into 'members' table
+    ''' 
+    \brief pushes a new entry into 'members' table
+    
+    \param _cursor   database cursor
+    \param _name     member's name
+    \param _group    member's group ID
     '''
     print("    pushing member")
     try:
         _cursor.execute("INSERT INTO members(name, group_of_members) values (?,?)", (_name, _group,))
+        return 0
     except sqlite3.Error as e:
-        print("An error occurred:", e.args[0])                 
+        print("An error occurred:", e.args[0])
+        return -1                 
 
 def push_into_matter_of_expense(_cursor, _name, _originator, _provider, _group, _amount):
-    ''' \brief pushes a new entry into 'members' table
+    ''' 
+    \brief pushes a new entry into 'matter of expense' table
     '''
     print("    pushing 'matter_of_expense'")
     try:
@@ -47,11 +78,17 @@ def push_into_matter_of_expense(_cursor, _name, _originator, _provider, _group, 
         print("An error occurred:", e.args[0])  
         
 def push_into_invoice(_cursor, _matter_of_expense, _originator, _date):
+    '''
+    \brief pushes a new entry into the 'invoice' table
+    '''
     print("    pushing 'invoices'")
     _cursor.execute("INSERT INTO invoices(matter_of_expense, originator, _date) values (?,?,?)", (_matter_of_expense, _originator, _date,))
     print("    dublicate album encountered")
 
 def push_into_groups_of_expenses(_cursor, _name):
+    '''
+    \brief pushes a new entry into the 'group of expenses' table
+    '''
     print("    pushing 'groups of expenses'")
     _cursor.execute("INSERT INTO groups_of_expenses(_name) values (?)", (_name,))
 
