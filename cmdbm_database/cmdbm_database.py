3f948744ccda48fd6302c3eb339f285cc15837f7
Benedikt Schmied <development@benedikt-schmied.de>
2018-01-14 09:15:27 +0100

    changing into object

--- cmdbm_database/cmdbm_database.py
+++ cmdbm_database/cmdbm_database.py
@@ -17,42 +17,53 @@ import sqlite3
 import os
 import argparse
 import bm_database
-import sys
+
+def menu_print():
+    print("print menu")
+    
+def menu_modify():
+    print("modify menu")
 
 def main():
     
+    menu = []
+    
+    menu_p = {'p', 'print', 'print a table', menu_print()} # use dictionary or list insted, no, make an object!
+    menu_c = {'m', 'modify', 'modify an entry', menu_modify()}
+    
+    
+    '''
+    this is the main menu, you can either tell us to 
+    print a table
+    insert a new entry into a table
+    or modify an entry within a table
+    '''
+    
+    menu.append(menu_p)
+    menu.append(menu_c)
+    
+    print(menu)
+    print(menu[0])
+    print(type(menu[0]))
+    
     while True:
+        
+        print("  ")
+        
         c = input("-->")
+        
         print(c)
-        if c == 'p':
-            break
+        if c == menu[0][0]:
             
-        elif c =='s':
-            break
+            menu[0][3]()            
+        elif c =='m':
+            menu_modify()
         elif c == 'i':
             
             d = bm_database.connect()
             bm_database.setup_db(d)
 
             bm_database.push_into_matter_of_expense(d, "Frisoer Benedikt", 1, 1, 1, 20, "monthly", 1)
-            break
-
-        elif c == 'r':
-            
-            d = bm_database.connect()
-            bm_database.setup_db(d)
-            
-            print(bm_database.get_entries_matter_of_expense(d))
-            
-            entry = bm_database.get_entries_matter_of_expense(d)
-            bm_database.disconnect(d)
-            a = 0
-            for cnti in entry:
-                str='{}'.format(cnti[0])
-                a = a + 2
-                
-            break
-
         elif c == 'q':
             break  # Exit the while loop
 
