fc78eac8581f5b947c6bc1fe76595db7510c017b
Benedikt Schmied <development@benedikt-schmied.de>
2018-02-22 18:34:56 +0100

    new files for testing the tables

---
+++ _test_bm_database/_test_bm_table_matter_of_expenses.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3 
+# coding=latin-1
+import sys
+
+sys.path.append('./../mod_logging_mkI_PYTHON')
+sys.path.append('./../_pro')
+sys.path.append('./../bm_database')
+from bm_globals import *
+import mod_logging_mkI_PYTHON
+from bm_table_matter_of_expenses import *
+
